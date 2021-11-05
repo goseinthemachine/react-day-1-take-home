8b4e3e5835a144ae1835896e2a1ee0484499e2a9
Cameron <cameron.gose@hey.com>
2021-11-04 20:45:24 -0700

    initial

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const getMoviesBySearchTerm = async (searchTerm) => {
     const url = new URL("http://www.omdbapi.com");
-    url.searchParams.append("apikey","878df137");
+    url.searchParams.append("apikey","enterhere");
     url.searchParams.append("s",searchTerm);
     const result = await fetch(url);
     return await result.json();
@@ -8,7 +8,7 @@ export const getMoviesBySearchTerm = async (searchTerm) => {
 
 export const getMovieDetailsById = async (id) => {
     const url = new URL("http://www.omdbapi.com");
-    url.searchParams.append("apikey","878df137");
+    url.searchParams.append("apikey","enterherer");
     url.searchParams.append("i",id);
     const result = await fetch(url);
     return result.json();
